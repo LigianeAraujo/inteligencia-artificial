7a8212107caae97bdb6be46e31581df33cacf229
LigianeAraujo <104834388+LigianeAraujo@users.noreply.github.com>
2024-08-14 15:06:09 -0300

    .

--- main.js
+++ main.js
@@ -1,125 +1,77 @@
-const caixaPrincipal= document.querySelector(".caixa-principal");
-const caixaPerguntas= document.querySelector(".caixa-perguntas");
-const caixaAlternativas= document.querySelector(".caixa-Alternativas");
-const caixaResultado= document.querySelector(".caixa-resultado");
-const textoResultado= document.querySelector(".texto-resultados");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 
-const perguntas =[
+const perguntas = [
     {
         enunciado: "Pergunta 1",
-        alternativas:[
+        alternativas: [
             {
                 texto: "Alternativa A",
                 afirmacao: "afirmacao da alternativa A."
             },
             {
-                 texto: "Alternativa B",
+                texto: "Alternativa B",
                 afirmacao: "afirmacao da alternativa B."
             }
         ]
     },
-
     {
         enunciado: "Pergunta 2",
-        alternativas:[
+        alternativas: [
             {
                 texto: "Alternativa A",
                 afirmacao: "afirmacao da alternativa A."
             },
             {
-                 texto: "Alternativa B",
+                texto: "Alternativa B",
                 afirmacao: "afirmacao da alternativa B."
             }
         ]
-    },
-
-    {
-        enunciado: "Pergunta 3",
-        alternativas:[
-            {
-                texto: "Alternativa A",
-                afirmacao: "afirmacao da alternativa A."
-            },
-            {
-                 texto: "Alternativa B",
-                afirmacao: "afirmacao da alternativa B."
-            }
-        ]
-    },
-
-    {
-        enunciado: "Pergunta 4",
-        alternativas:[
-            {
-                texto: "Alternativa A",
-                afirmacao: "afirmacao da alternativa A."
-            },
-            {
-                 texto: "Alternativa B",
-                afirmacao: "afirmacao da alternativa B."
-            }
-        ]
-    },
-
-    {
-        enunciado: "Pergunta 5",
-        alternativas:[
-            {
-                texto: "Alternativa A",
-                afirmacao: "afirmacao da alternativa A."
-            },
-            {
-                 texto: "Alternativa B",
-                afirmacao: "afirmacao da alternativa B."
-            }
-        ]
-    },
+    }
 ];
 
- let atual = 0;
- let perguntaAtual;
- let historiaFinal = "";
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
 
- function mostraPergunta(){
+function mostraPergunta(){
     if (atual >= perguntas.length){
-        mostraReseltado();
+        mostraResultado();t
         return;
     }
-
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
-    mostraAlternativa();
+    mostraAlternativas();
+}
 
-    function mostraPergunta(){
-        for(const alternativa of perguntaAtual.alternativas){
-            const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener("click",
-            ()=>respostaSelecionado(alternativa));
-            caixaAlternativas.appendChild(botaoAlternativas);
-        }
-     }
-     function respostaSelecionada(opcaoSelecionada) {
-        const afirmacoes = opcaoSelecionada.afirmacao;
-        historiaFinal += afirmacoes + " ";
-        atual ++;
-        mostraPergunta();
-      }
-     function mostraReseltado(){
-        caixaPerguntas.textContent = "De Acordo com suas respostas...";
-        textoResultado.textContent = historiaFinal;
-        caixaAlternativas.textContent = "";
-     }
-
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",
+            ()=>respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual ++;
     mostraPergunta();
+}
 
 
 
 
+function mostraResultado(){
+    caixaPerguntas.textContent = "De acordo com suas respostas...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
-
- }
-
-
+mostraPergunta();
